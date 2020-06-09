47bb55fccf47280ece80ac52851d4ad38a38b518
zhengyunbin <253172157@qq.com>
2020-06-09 22:31:56 +0800

    v1.0.0

--- app/src/main/java/com/example/gittext/MainActivity.java
+++ app/src/main/java/com/example/gittext/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG,"master2");
+        Log.i(TAG,"master2");
+        Log.i(TAG,"master2");
     }
 }
